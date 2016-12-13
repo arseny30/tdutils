0885394621564614f0420c2ad08a15860269e20e
Arseny Smirnov <arseny30@gmail.com>
2016-12-13 16:41:26 +0300

    tdutils: copy files bugfix

--- td/utils/filesystem.cpp
+++ td/utils/filesystem.cpp
@@ -1,5 +1,6 @@
 #include "td/utils/port/FileFd.h"
 #include "td/utils/buffer.h"
+#include "td/utils/format.h"
 #include "td/utils/Status.h"
 #include "td/utils/Slice.h"
 
@@ -21,10 +22,13 @@ Result<BufferSlice> read_file(CSlice path, off_t size) {
 // Very straightforward function. Don't expect much of it.
 Status copy_file(CSlice from, CSlice to, off_t size) {
   TRY_RESULT(content, read_file(from, size));
+  if (size == -1) {
+    size = content.size();
+  }
   TRY_RESULT(to_file, FileFd::open(to, FileFd::Truncate | FileFd::Create | FileFd::Write));
   TRY_RESULT(written, to_file.write(content.as_slice()));
   if (written != static_cast<size_t>(size)) {
-    return Status::Error("Failed to write file");
+    return Status::Error(PSTR() << "Failed to write file" << tag("size", size) << tag("written", written));
   }
   to_file.close();
   return Status::OK();
