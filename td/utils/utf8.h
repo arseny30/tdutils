26f509c44eab16597387a71da6d70094ac593c15
levlam <levlam@telegram.org>
2017-04-24 23:51:35 +0300

    Much better match_url.

--- td/utils/utf8.h
+++ td/utils/utf8.h
@@ -6,6 +6,7 @@ namespace td {
 
 // checks UTF-8 string for correctness
 bool check_utf8(CSlice str);
+
 inline const unsigned char *prev_utf8_unsafe(const unsigned char *ptr) {
   while (((*--ptr) & 0xc0) == 0x80) {
     // pass
@@ -22,17 +23,17 @@ inline const unsigned char *next_utf8_unsafe(const unsigned char *ptr, uint32 *c
     return ptr + 1;
   } else if ((a & 0x20) == 0) {
     if (code) {
-      *code = (a << 6) | (ptr[1] & 0x3f);
+      *code = ((a & 0x1f) << 6) | (ptr[1] & 0x3f);
     }
     return ptr + 2;
   } else if ((a & 0x10) == 0) {
     if (code) {
-      *code = (a << 12) | ((ptr[1] & 0x3f) << 6) | (ptr[2] & 0x3f);
+      *code = ((a & 0x0f) << 12) | ((ptr[1] & 0x3f) << 6) | (ptr[2] & 0x3f);
     }
     return ptr + 3;
   } else if ((a & 0x08) == 0) {
     if (code) {
-      *code = (a << 18) | ((ptr[1] & 0x3f) << 12) | ((ptr[2] & 0x3f) << 6) | (ptr[3] & 0x3f);
+      *code = ((a & 0x07) << 18) | ((ptr[1] & 0x3f) << 12) | ((ptr[2] & 0x3f) << 6) | (ptr[3] & 0x3f);
     }
     return ptr + 4;
   }
