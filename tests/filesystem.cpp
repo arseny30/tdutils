817b41fd9d51be4caa3c7b42e28c6b8961197a58
levlam <levlam@telegram.org>
2017-11-27 03:49:44 +0300

    Move Td::set_td_parameters to on_request.

--- tests/filesystem.cpp
+++ tests/filesystem.cpp
@@ -14,14 +14,23 @@ TEST(Misc, clean_filename) {
   ASSERT_STREQ(clean_filename("     .    "), "");
   ASSERT_STREQ(clean_filename("!@#$%^&*()_+-=[]{;|:\"}'<>?,.`~"), "!@#$%^  ()_+-=[]{;   }    ,.~");
   ASSERT_STREQ(clean_filename("!@#$%^&*()_+-=[]{}\\|:\";'<>?,.`~"), ";    ,.~");
-  ASSERT_STREQ(clean_filename("عرفها بعد قد. هذا مع تاريخ اليميني واندونيسيا،, لعدم تاريخ لهيمنة الى"), "عرفها بعد قد.هذا مع تاريخ اليميني");
-  ASSERT_STREQ(clean_filename("012345678901234567890123456789012345678901234567890123456789adsasdasdsaa.01234567890123456789asdasdasdasd"), "012345678901234567890123456789012345678901234567890123456789.01234567890123456789");
-  ASSERT_STREQ(clean_filename("01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   0123456789`<><<>><><>0123456789asdasdasdasd"), "01234567890123456789012345678901234567890123456789.0123456789");
-  ASSERT_STREQ(clean_filename("01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   0123456789`<><><>0123456789asdasdasdasd"), "01234567890123456789012345678901234567890123456789.0123456789       012");
+  ASSERT_STREQ(clean_filename("عرفها بعد قد. هذا مع تاريخ اليميني واندونيسيا،, لعدم تاريخ لهيمنة الى"),
+               "عرفها بعد قد.هذا مع تاريخ اليميني");
+  ASSERT_STREQ(
+      clean_filename(
+          "012345678901234567890123456789012345678901234567890123456789adsasdasdsaa.01234567890123456789asdasdasdasd"),
+      "012345678901234567890123456789012345678901234567890123456789.01234567890123456789");
+  ASSERT_STREQ(clean_filename("01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   "
+                              "0123456789`<><<>><><>0123456789asdasdasdasd"),
+               "01234567890123456789012345678901234567890123456789.0123456789");
+  ASSERT_STREQ(clean_filename("01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   "
+                              "0123456789`<><><>0123456789asdasdasdasd"),
+               "01234567890123456789012345678901234567890123456789.0123456789       012");
   ASSERT_STREQ(clean_filename("C:/document.tar.gz"), "document.tar.gz");
   ASSERT_STREQ(clean_filename("test...."), "test");
   ASSERT_STREQ(clean_filename("....test"), "test");
-  ASSERT_STREQ(clean_filename("test.exe...."), "test.exe"); // extension has changed
-  ASSERT_STREQ(clean_filename("test.exe01234567890123456789...."), "test.exe01234567890123456789"); // extension may be more then 20 characters
+  ASSERT_STREQ(clean_filename("test.exe...."), "test.exe");  // extension has changed
+  ASSERT_STREQ(clean_filename("test.exe01234567890123456789...."),
+               "test.exe01234567890123456789");  // extension may be more then 20 characters
   ASSERT_STREQ(clean_filename("....test....asdf"), "test.asdf");
 }
