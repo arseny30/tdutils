3d25e3bcca8363e2bdc9776d3894349ffe1dcc66
levlam <levlam@telegram.org>
2017-12-05 11:54:41 +0300

    overloaded.h.

--- td/utils/misc.h
+++ td/utils/misc.h
@@ -370,31 +370,4 @@ bool is_aligned_pointer(const T *pointer) {
   return (reinterpret_cast<std::uintptr_t>(static_cast<const void *>(pointer)) & (Alignment - 1)) == 0;
 }
 
-// overloaded
-namespace detail {
-template <class... Fs>
-struct overload;
-
-template <class F>
-struct overload<F> : public F {
-  explicit overload(F f) : F(f) {
-  }
-};
-
-template <class F, class... Fs>
-struct overload<F, Fs...>
-    : public overload<F>
-    , overload<Fs...> {
-  overload(F f, Fs... fs) : overload<F>(f), overload<Fs...>(fs...) {
-  }
-  using overload<F>::operator();
-  using overload<Fs...>::operator();
-};
-}  // namespace detail
-
-template <class... F>
-auto overloaded(F... f) {
-  return detail::overload<F...>(f...);
-}
-
 }  // namespace td
