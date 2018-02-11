41ba347e20c55199146cce244b70689edb6ad900
levlam <levlam@telegram.org>
2018-02-11 16:05:51 +0300

    Use to_integer_safe instead of sscanf in mem_stat.

--- td/utils/port/Stat.h
+++ td/utils/port/Stat.h
@@ -27,10 +27,10 @@ Stat fstat(int native_fd);  // TODO return Result<Stat>
 Status update_atime(CSlice path) TD_WARN_UNUSED_RESULT;
 
 struct MemStat {
-  uint64 resident_size_;
-  uint64 resident_size_peak_;
-  uint64 virtual_size_;
-  uint64 virtual_size_peak_;
+  uint64 resident_size_ = 0;
+  uint64 resident_size_peak_ = 0;
+  uint64 virtual_size_ = 0;
+  uint64 virtual_size_peak_ = 0;
 };
 
 Result<MemStat> mem_stat() TD_WARN_UNUSED_RESULT;
