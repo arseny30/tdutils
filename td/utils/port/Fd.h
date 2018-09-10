5dc123e227f849ca872b33bf8c402a0ac7bfa89b
levlam <levlam@telegram.org>
2018-09-10 17:47:28 +0300

    Reimplement NativeFd::duplicate.

--- td/utils/port/Fd.h
+++ td/utils/port/Fd.h
@@ -1,17 +1,4 @@
 #pragma once
 
 #if 0
-#include "td/utils/port/config.h"
-
-#include "td/utils/common.h"
-#include "td/utils/Status.h"
-
-namespace td {
-
-class Fd {
- public:
-  static Status duplicate(const Fd &from, Fd &to);
-};
-
-}  // namespace td
 #endif
