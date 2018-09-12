ce76506ff3816cca99b81fe462b77dc551e2b0d4
Arseny Smirnov <arseny30@gmail.com>
2018-09-12 15:07:54 +0300

    Update test for walk_path. It is CE now

--- td/utils/port/StdStreams.cpp
+++ td/utils/port/StdStreams.cpp
@@ -35,7 +35,6 @@ static FileFd &get_file_fd() {
   static FileFd result = FileFd::from_native_fd(NativeFd(handle, true));
 #else
   static FileFd result;
-  result = FileFd();
 #endif
   return result;
 }
