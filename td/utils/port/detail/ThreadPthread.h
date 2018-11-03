c37457e82b24a0a8fc21f13ee4c1c247e191e8b4
levlam <levlam@telegram.org>
2018-11-03 22:36:52 +0300

    Fix ThreadPthread.

--- td/utils/port/detail/ThreadPthread.h
+++ td/utils/port/detail/ThreadPthread.h
@@ -30,6 +30,7 @@ class ThreadPthread {
     join();
     is_inited_ = std::move(other.is_inited_);
     thread_ = other.thread_;
+    return *this;
   }
   template <class Function, class... Args>
   explicit ThreadPthread(Function &&f, Args &&... args) {
@@ -76,7 +77,6 @@ class ThreadPthread {
   static void *run_thread(void *ptr) {
     ThreadIdGuard thread_id_guard;
     auto func = unique_ptr<Destructor>(static_cast<Destructor *>(ptr));
-    func->reset();
     return nullptr;
   }
 };
