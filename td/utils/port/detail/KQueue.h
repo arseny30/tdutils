53570c935f7a1383656157b08d5ba7e19b6bfc2e
levlam <levlam@telegram.org>
2018-11-27 04:24:43 +0300

    Improve clean_filename behavior.

--- td/utils/port/detail/KQueue.h
+++ td/utils/port/detail/KQueue.h
@@ -12,7 +12,7 @@
 
 #include <cstdint>
 
-#include <sys/types.h> // must be included before sys/event.h, which depends on sys/types.h on FreeBSD
+#include <sys/types.h>  // must be included before sys/event.h, which depends on sys/types.h on FreeBSD
 
 #include <sys/event.h>
 
