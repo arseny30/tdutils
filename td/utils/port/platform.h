b77997587fbf6eb932b5a8dfd56a22ac1932664d
levlam <levlam@telegram.org>
2018-12-01 04:15:59 +0300

    Modernize spelling of macOs.

--- td/utils/port/platform.h
+++ td/utils/port/platform.h
@@ -14,7 +14,7 @@
 #elif defined(__APPLE__)
   #include "TargetConditionals.h"
   #if TARGET_OS_IPHONE
-    // iOS/Apple Watch OS/Apple TV OS
+    // iOS/watchOS/tvOS
     #if TARGET_OS_IOS
       #define TD_DARWIN_IOS 1
     #elif TARGET_OS_TV
@@ -25,7 +25,7 @@
       #warning "Probably unsupported Apple iPhone platform. Feel free to try to compile"
     #endif
   #elif TARGET_OS_MAC
-    // Other kinds of Mac OS
+    // Other kinds of macOS
     #define TD_DARWIN_MAC 1
   #else
     #warning "Probably unsupported Apple platform. Feel free to try to compile"
