5aefa9393089b2919fa8293a3f7ef252e63d5148
levlam <levlam@telegram.org>
2019-07-22 07:05:50 +0300

    Fix GCC CE and warnings.

--- td/utils/port/stacktrace.h
+++ td/utils/port/stacktrace.h
@@ -6,6 +6,8 @@ class Stacktrace {
  public:
   struct PrintOptions {
     bool use_gdb = false;
+    PrintOptions() {
+    }
   };
   static void print_to_stderr(const PrintOptions &options = PrintOptions());
 };
