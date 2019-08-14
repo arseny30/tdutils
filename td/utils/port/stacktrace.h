3054fc6d5f779aa282934351e9cf6d242ddbe0e8
Arseny Smirnov <arseny30@gmail.com>
2019-08-14 11:31:25 +0300

    Stacktrace improvements.

--- td/utils/port/stacktrace.h
+++ td/utils/port/stacktrace.h
@@ -10,6 +10,8 @@ class Stacktrace {
     }
   };
   static void print_to_stderr(const PrintOptions &options = PrintOptions());
+  // backtrace needs to be called once to ensure that next calls are async-signal-safe
+  static void init();
 };
 
 }  // namespace td
