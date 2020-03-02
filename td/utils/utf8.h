3e63c07b8c7ca3db570718187b76b44d71e8913b
levlam <levlam@telegram.org>
2020-03-02 16:05:18 +0300

    Make webPage.description a formatted text.

--- td/utils/utf8.h
+++ td/utils/utf8.h
@@ -83,6 +83,9 @@ T utf8_utf16_truncate(T str, size_t length) {
 
 template <class T>
 T utf8_substr(T str, size_t offset) {
+  if (offset == 0) {
+    return str;
+  }
   auto offset_pos = utf8_truncate(str, offset).size();
   return str.substr(offset_pos);
 }
@@ -94,6 +97,9 @@ T utf8_substr(T str, size_t offset, size_t length) {
 
 template <class T>
 T utf8_utf16_substr(T str, size_t offset) {
+  if (offset == 0) {
+    return str;
+  }
   auto offset_pos = utf8_utf16_truncate(str, offset).size();
   return str.substr(offset_pos);
 }
