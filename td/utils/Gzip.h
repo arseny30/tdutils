9c264843fba90851cfca06178c8ce43766e43a52
levlam <levlam@telegram.org>
2020-03-15 01:56:48 +0300

    Make max_compression_ratio required parameter in gzencode.

--- td/utils/Gzip.h
+++ td/utils/Gzip.h
@@ -93,7 +93,7 @@ class Gzip {
 
 BufferSlice gzdecode(Slice s);
 
-BufferSlice gzencode(Slice s, double k = 0.9);
+BufferSlice gzencode(Slice s, double max_compression_ratio);
 
 }  // namespace td
 
